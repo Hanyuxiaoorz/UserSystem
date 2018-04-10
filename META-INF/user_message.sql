a217f91c303390a7d5190f4a0345eb508a3ec287
邓思远 <2208864697@qq.com>
2018-04-10 16:04:38 +0800

    feat()添加了用户注册数据库

---
+++ META-INF/user_message.sql
@@ -0,0 +1,21 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.21-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `user_mess` (
+	`username` varchar (150),
+	`password` varchar (18),
+	`id` double ,
+	`e-mail` varchar (150),
+	`sex` varchar (3),
+	`phone_number` double ,
+	`major` varchar (150),
+	`class_number` double ,
+	`study_direction` varchar (150),
+	`age` double ,
+	`birth` date ,
+	`habit` varchar (150)
+); 
