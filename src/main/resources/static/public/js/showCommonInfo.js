12b4e0e844f0addd3dc101805a531fd7e064beb5
libowen <1064416560@qq.com>
2018-08-17 17:15:34 +0800

    :sparkles:feat:()添加了点击用户名和头像跳转到个人信息界面的功能

--- src/main/resources/static/public/js/showCommonInfo.js
+++ src/main/resources/static/public/js/showCommonInfo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     getImg();
+    $('#userImg').click(function () {
+        toPersonPage($('#userName').text());
+    });
+    $('#userName').click(function () {
+        toPersonPage($('#userName').text());
+    })
 });
 
 //获取用户头像
@@ -70,4 +76,9 @@ function logOut(){
             console.log(thrownError);
         }
     });
+}
+
+//点击用户名和头像跳转
+function  toPersonPage(username){
+    window.location.href="personage.html?username" + username;
 }
